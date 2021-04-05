0f4464b1f9c77e74a8357607f938ff46b66aa101
Mohamed Jamal Ouazani Touhami <m.jamal.115@gmail.com>
2021-04-05 14:22:19 +0100

    Update index.jsx
    Hello,
    
    I make some modification on index.jsx,
    Please check them.
    
    Regards,
    Jamal

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,14 @@ import { hot } from "react-hot-loader"
 import { render } from "react-dom"
 import "./style"
 
-const App = () => <h3>Hello World</h3>
+const App = () => {
+  return (
+    <div>
+      <h3>Hello World</h3>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Florida_Box_Turtle_Digon3_re-edited.jpg/1200px-Florida_Box_Turtle_Digon3_re-edited.jpg"/>
+    </div>
+  )
+}
 
 export default hot(module)(App)
 
